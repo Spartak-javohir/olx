f68a92ac53702c3a5590b14f2ca44823bd7c8883
Spartak-javohir <dellspartak0@gmail.com>
2021-09-24 17:22:39 +0500

    server change

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-require("dotenv").config()
+require("dotenv").config();
 
-
+const cookieParser = require("cookie-parser");
 const express = require("express");
-const server = express();
+const morgan = require("morgan");
+const path = require("path");
+const PORT = process.env.PORT || 8080;
 
-const PORT =  process.env.PORT || 8080
+async function server() {
+  const server = express();
 
-server.listen(PORT, {
-    console.log('server on 8080')
-});
+  server.use(express.json());
+  server.use(express.urlencoded());
+  server.use(morgan("dev"));
+  server.use(cookieParser);
+  serve.use(express.static(path.join(__dirname, "src", "public")));
+}
+
+server.listen(PORT);
